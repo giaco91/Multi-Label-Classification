c8b2ee5964c4d6bc17155925242608fc43b2a7b9
giaco <muzlu@hotmail.com>
2016-12-07 16:21:41 +0100

    changes

--- MLP3.m
+++ MLP3.m
@@ -7,45 +7,53 @@ method='LRlasso';%NN,SVM,LR,LRlasso,DA
 class=3;%1=sex,2=age,3=health
 cv=10;%0 for no cv
 
-%read in
+%read in target-vector
 y=csvread('targets.csv');
 y_train=y(:,class);
 
+%extract features
+
+%hypocampus
 hv=Feature_select('train',[49 71;100 112;53 65],'voxvar',0,0);
 hdm=Feature_select('train',[49 71;100 112;53 65],'ivox',0,[20 140]);
 hbm=Feature_select('train',[49 71;100 112;53 65],'ivox',0,[420 1420]);
 hdv=Feature_select('train',[49 71;100 112;53 65],'ivoxvar',0,[20 140]);
 hbv=Feature_select('train',[49 71;100 112;53 65],'ivoxvar',0,[420 1420]);
 
+%inner ventricle
 iv=Feature_select('train',[67 108;116 144;77 90],'voxvar',0,0);
 idm=Feature_select('train',[67 108;116 144;77 90],'ivox',0,[20 120]);
 ibm=Feature_select('train',[67 108;116 144;77 90],'ivox',0,[920 1721]);
 idv=Feature_select('train',[67 108;116 144;77 90],'ivoxvar',0,[20 120]);
 ibv=Feature_select('train',[67 108;116 144;77 90],'ivoxvar',0,[920 1721]);
 
+%overall
 av=Feature_select('train',[1 89;1 208;1 176],'voxvar',0,0);
 adm=Feature_select('train',[1 89;1 208;1 176],'ivox',0,[70 320]);
 abm=Feature_select('train',[1 89;1 208;1 176],'ivox',0,[920 1420]);
 adv=Feature_select('train',[1 89;1 208;1 176],'ivoxvar',0,[70 320]);
 abv=Feature_select('train',[1 89;1 208;1 176],'ivoxvar',0,[920 1420]);
 
-    X=[hv hdm hbm hdv hbv iv idm ibm idv ibv av adm abm adv abv];
-    X=preprocess(X,y,'train','norm','id');
-    X_train=X;
-    if submission==1
-
+X=[hv hdm hbm hdv hbv iv idm ibm idv ibv av adm abm adv abv];
+X=preprocess(X,y,'train','norm','id');
+X_train=X;
+if submission==1
+        
+        %hypocampus
         thv=Feature_select('test',[49 71;100 112;53 65],'voxvar',0,0);
         thdm=Feature_select('test',[49 71;100 112;53 65],'ivox',0,[20 140]);
         thbm=Feature_select('test',[49 71;100 112;53 65],'ivox',0,[420 1420]);
         thdv=Feature_select('test',[49 71;100 112;53 65],'ivoxvar',0,[20 140]);
         thbv=Feature_select('test',[49 71;100 112;53 65],'ivoxvar',0,[420 1420]);
         
+        %inner ventricle
         tiv=Feature_select('test',[67 108;116 144;77 90],'voxvar',0,0);
         tidm=Feature_select('test',[67 108;116 144;77 90],'ivox',0,[20 120]);
         tibm=Feature_select('test',[67 108;116 144;77 90],'ivox',0,[920 1721]);
         tidv=Feature_select('test',[67 108;116 144;77 90],'ivoxvar',0,[20 120]);
         tibv=Feature_select('test',[67 108;116 144;77 90],'ivoxvar',0,[920 1721]);
 
+        %over all
         tav=Feature_select('test',[1 89;1 208;1 176],'voxvar',0,0);
         tadm=Feature_select('test',[1 89;1 208;1 176],'ivox',0,[70 320]);
         tabm=Feature_select('test',[1 89;1 208;1 176],'ivox',0,[920 1420]);
@@ -56,16 +64,16 @@ abv=Feature_select('train',[1 89;1 208;1 176],'ivoxvar',0,[920 1420]);
         Y=[thv thdm thbm thdv thbv tiv tidm tibm tidv tibv tav tadm tabm tadv tabv];
         Y=preprocess(Y,'void','test','norm','id');
         X_test=Y;
-    end
+end
     
-    [ClObj void perf]=classifier(X_train,y_train,submission,method,cv,'train','void');
-    CV_mean_med_std=perf'
-    if submission==1
+[ClObj void perf]=classifier(X_train,y_train,submission,method,cv,'train','void');
+CV_mean_med_std=perf'
+if submission==1
         [void yhat void]=classifier(X_test,'void',submission,method,cv,'test',ClObj);
         header={'"ID"','"Prediction"'};
         submission_data=[[1:length(yhat)]',yhat];
         csvwrite_with_headers('submission.csv',submission_data,header)
-    end
+end
 
 
 if size(X_train,2)==2
